59df075d6745b28075c6b4b6d1fd564fcf79f440
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-01-25 13:08:05 +0100

    Created basic calculator functionality

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,55 @@
 <template>
 <div class="container">
-    <div class="target">
-      
+  <div class="target wrapper">
+    <div class="display">
+      <p id="Equation">{{ equation }}</p>
     </div>
-    <Moveable
-        className="moveable"
-        v-bind:target="['.target']"
-        v-bind:draggable="true"
-        v-bind:scalable="true"
-        @drag="onDrag"
-        @scale="onScale"
-    />
+    <calculator-button @clicked="onCalcBtnClick" val="C" />
+    <calculator-button @clicked="onCalcBtnClick" val="ANS" />
+    <calculator-button @clicked="onCalcBtnClick" val="DEL" />
+    <calculator-button @clicked="onCalcBtnClick" val=" + " />
+    <calculator-button @clicked="onCalcBtnClick" val="1" />
+    <calculator-button @clicked="onCalcBtnClick" val="2" />
+    <calculator-button @clicked="onCalcBtnClick" val="3" />
+    <calculator-button @clicked="onCalcBtnClick" val=" - " />
+    <calculator-button @clicked="onCalcBtnClick" val="4" />
+    <calculator-button @clicked="onCalcBtnClick" val="5" />
+    <calculator-button @clicked="onCalcBtnClick" val="6" />
+    <calculator-button @clicked="onCalcBtnClick" val=" * " />
+    <calculator-button @clicked="onCalcBtnClick" val="7" />
+    <calculator-button @clicked="onCalcBtnClick" val="8" />
+    <calculator-button @clicked="onCalcBtnClick" val="9" />
+    <calculator-button @clicked="onCalcBtnClick" val=" / " />
+    <calculator-button />
+    <calculator-button @clicked="onCalcBtnClick" val="0" />
+    <calculator-button @clicked="onCalcBtnClick" val="." />
+    <calculator-button @clicked="onCalcBtnClick" val="=" />
+  </div>
+  <Moveable
+      className="moveable"
+      v-bind:target="['.target']"
+      v-bind:draggable="true"
+      v-bind:scalable="true"
+      @drag="onDrag"
+      @scale="onScale"
+  />
 </div>
 </template>
 
 <script>
 import Moveable from 'vue3-moveable'
+import CalculatorButton from './components/CalculatorButton.vue'
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      equation: "",
+      ans: 0
+    }
+  },
   components: {
+    CalculatorButton,
     Moveable,
   },
   methods: {
@@ -29,6 +59,21 @@ export default {
     onScale({ target, drag }) {
       target.style.transform = drag.transform
     },
+    onCalcBtnClick(val) {
+      if (val === "=") {
+        let ans = eval(this.equation)
+        this.equation = ans.toString()
+        this.ans = ans
+      }
+      else if (val === "C")
+        this.equation = ""
+      else if (val === "DEL")
+        this.equation = this.equation.slice(0, -1)
+      else if (val === "ANS")
+        this.equation += this.ans
+      else 
+        this.equation += val
+    }
   }
 }
 </script>
@@ -42,4 +87,20 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
+.display {
+  grid-column: 5/1;
+  height: 70px;
+}
+
+.display > p {
+  float: right;
+  margin-right: 25px;
+}
+
 </style>
---
+++ src/components/CalculatorButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button
+        class="calc-button"
+        @click="$emit('clicked', val)">
+            {{ val }}
+        </button>
+</template>
+
+<script>
+
+export default {
+    props: {
+        val: {
+            type: [Number, String],
+            default: " "
+        },
+    }
+}
+</script>
+
+<style scoped>
+.calc-button {
+    padding: 5px;
+}
+</style>
